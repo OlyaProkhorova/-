17efb8b7f13f39296790dadd38d30cda02f6b3ce
OlyaProkhorova <lolyalyolya@gmail.com>
2023-03-24 17:54:19 +0300

    updated code

--- info/views.py
+++ info/views.py
@@ -20,3 +20,20 @@ class Info10CoinsPage(ListView):
     def get_queryset(self):
         return get_crypto_data_from_coin_gecko(False)
 
+class Info250CoinsPage(ListView):
+    template_name = "info_250_coins_page.html"
+    context_object_name = "crypto_info"
+    paginate_by = 25
+
+    # функция не позволяет открыть страницу, если пользовать не авторизирован
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            # reverse позволяет использовать сокращенное имя, а не путь
+            return redirect(reverse('welcome_page'))
+        return super().dispatch(request, *args, **kwargs)
+
+    # возвращает список данных, состоящий из словарей,
+    # где каждый словарь - информация об одной криптомонете.
+    # далее этот список используется в файле html в теге for
+    def get_queryset(self):
+        return get_crypto_data_from_coin_gecko(True)
