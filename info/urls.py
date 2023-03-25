bf741ba109e4e91e0f07499c789f84bbe3c70efd
OlyaProkhorova <lolyalyolya@gmail.com>
2023-03-25 10:08:51 +0300

    added info/urls.py

--- info/urls.py
+++ info/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import WelcomePage
+from .views import WelcomePage, Info10CoinsPage, Info250CoinsPage
 
 urlpatterns = [
-    path("", WelcomePage.as_view(), name="welcome_page")
+    path("", WelcomePage.as_view(), name="welcome_page"),
+    path("10coins/", Info10CoinsPage.as_view(), name="info_10_coins"),
+    path("250coins/", Info250CoinsPage.as_view(), name="info_250_coins")
 ]
