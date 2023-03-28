e3416c8c9e9e133233e7203b343a32c3e22315eb
OlyaProkhorova <lolyalyolya@gmail.com>
2023-03-28 17:37:16 +0300

    modified login.html, signup.html and added views.py

--- authorization/views.py
+++ authorization/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def signup_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        new_user = User.objects.create_user(username, email, password)
+        new_user.save()
+        user = authenticate(request, username=username, password=password)
+        login(request, user)
+        return redirect('info_250_coins')
+    return render(request, 'registration/signup.html')
+
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('info_250_coins')
+    return render(request, 'registration/login.html')
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('welcome_page')
--- cryptualno/urls.py
+++ cryptualno/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from authorization.views import login_page, signup_page, logout_page
+
 
 urlpatterns = [
     path("", include("info.urls")),
+    path('signup/', signup_page, name="signup"),
+    path('login/', login_page, name="login"),
+    path('logout/', logout_page, name="logout"),
     path('admin/', admin.site.urls),
 ]
